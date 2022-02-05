8812bb85fea02c36f4f42f65fcc9097d21fed444
michaelzusev <mike.zusev@gmail.com>
2022-02-05 12:36:39 -0500

    Add breakdown and x axis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,15 @@ import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from '@mui/material/LinearProgress';
 import Select from "react-select";
+import Grid from "@mui/material/Grid";
 
 //Nivo
 import { ResponsiveBar } from "@nivo/bar";
 
 //Data
-import groupBy from "json-groupby";
-import { tidy, pivotWider, filter } from "@tidyjs/tidy";
+import { tidy, pivotWider } from "@tidyjs/tidy";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -29,11 +29,36 @@ const App = () => {
       },
     ],
     department: [{ label: "Public Service", value: "Public Service" }],
-    demographic: [{label: 'Female gender', value: 'Female gender'},
-                  {label: 'Male gender', value: 'Male gender'}]
+    demographic: [
+      { label: "Female gender", value: "Female gender" },
+      { label: "Male gender", value: "Male gender" },
+    ],
   });
 
-  const [pivot, setPivot] = useState();
+  const [pivot, setPivot] = useState({
+    label: "Demographic",
+    value: "demographic",
+  });
+  const [axis, setAxis] = useState({ label: "Question", value: "question" });
+
+  const pivotOptions = [
+    {
+      label: "Demographic",
+      value: "demographic",
+    },
+    {
+      label: "Survey Year",
+      value: "survey_year",
+    },
+    {
+      label: "Question",
+      value: "question",
+    },
+    {
+      label: "Department",
+      value: "department",
+    },
+  ];
 
   useEffect(() => {
     fetch("data/clean.json")
@@ -54,7 +79,6 @@ const App = () => {
       filters.demographic.map((obj) => obj.value).includes(point.demographic)
   );
 
-
   const pivotDataGroup = pivot
     ? tidy(
         filteredData.map((obj) => ({
@@ -62,7 +86,7 @@ const App = () => {
           demographic: obj.demographic,
           question: obj.question,
           department: obj.department,
-          score:obj.score
+          score: obj.score,
         })),
         pivotWider({
           namesFrom: pivot.value,
@@ -75,6 +99,21 @@ const App = () => {
     { value: 2020, label: "2020" },
     { value: 2019, label: "2019" },
     { value: 2018, label: "2018" },
+  ];
+
+  const demographic_options = [
+    {
+      value: "Male gender",
+      label: "Male gender",
+    },
+    {
+      value: "Female gender",
+      label: "Female gender",
+    },
+    {
+      value: "Indigenous",
+      label: "Non-Indigenous",
+    },
   ];
 
   const question_options = [
@@ -108,11 +147,25 @@ const App = () => {
     }));
   };
 
-  const handlePivotChange = (event_object) => {
-    setPivot(event_object)
-  }
+  const handleDemographicChange = (event_array) => {
+    console.log(event_array);
+    setFilters((prevFilter) => ({
+      ...prevFilter,
+      demographic: event_array,
+    }));
+  };
 
-  console.log(pivot)
+  const handlePivotChange = (event_object) => {
+    setPivot(event_object);
+  };
+
+  const handleAxisChange = (event_object) => {
+    setAxis(event_object);
+  };
+
+  console.log(pivot);
+
+  const padding = 0;
 
   return (
     <Container>
@@ -123,40 +176,125 @@ const App = () => {
         <Typography variant="h5" component="h2" gutterBottom>
           Retention and mobilization of talent among employment equity groups
         </Typography>
-        {data.length === 0 && <CircularProgress />}
-        <Select
-          options={Object.keys(filters).map(filter => ({label:filter, value:filter}))}
-          value={pivot ? pivot : []}
-          onChange={handlePivotChange}
-        />
-        <Select
-          isMulti
-          options={survey_year_options}
-          value={filters.survey_year}
-          onChange={handleSurveyYearChange}
-        />
-        <Select
-          isMulti
-          options={question_options}
-          value={filters.question}
-          onChange={handleQuestionChange}
-        />
-        <div style={{ height: 200, width: "100%" }}>
-          <ResponsiveBar
-            data={pivotDataGroup}
-            keys={pivot ? filters[pivot.value].map((obj) => obj.value) : ["score"]}
-            indexBy="demographic"
-            groupMode='grouped'
-            tooltip = {(point) => (
-              <div
-                style={{maxWidth:400, background:'white'}}>
-                  <p>{point.id}:{point.value}</p>
+
+        <Grid container spacing={1}>
+          <Grid container item xs={2} alignSelf="center" padding={padding}>
+            <Typography variant="p" component="p">
+              Breakdown
+            </Typography>
+          </Grid>
+          <Grid item xs={10} padding={padding}>
+            <Select
+              options={pivotOptions}
+              value={pivot ? pivot : []}
+              onChange={handlePivotChange}
+            />
+          </Grid>
+          <Grid container item xs={2} alignSelf="center" padding={padding}>
+            <Typography variant="p" component="p">
+              X Axis
+            </Typography>
+          </Grid>
+          <Grid item xs={10} padding={padding}>
+            <Select
+              options={pivotOptions}
+              value={axis ? axis : []}
+              onChange={handleAxisChange}
+            />
+          </Grid>
+          <Grid container item xs={2} alignSelf="center" padding={padding}>
+            <Typography variant="p" component="p">
+              Survey Year
+            </Typography>
+          </Grid>
+          <Grid item xs={10} padding={padding}>
+            <Select
+              isMulti
+              options={survey_year_options}
+              value={filters.survey_year}
+              onChange={handleSurveyYearChange}
+            />
+          </Grid>
+          <Grid container item xs={2} alignSelf="center" padding={padding}>
+            <Typography variant="p" component="p">
+              Question
+            </Typography>
+          </Grid>
+          <Grid item xs={10} padding={padding}>
+            <Select
+              isMulti
+              options={question_options}
+              value={filters.question}
+              onChange={handleQuestionChange}
+            />
+          </Grid>
+          <Grid container item xs={2} alignSelf="center" padding={padding}>
+            <Typography variant="p" component="p">
+              Demographic
+            </Typography>
+          </Grid>
+          <Grid item xs={10} padding={padding}>
+            <Select
+              isMulti
+              options={demographic_options}
+              value={filters.demographic}
+              onChange={handleDemographicChange}
+            />
+          </Grid>
+        </Grid>
+
+        {data.length ===0 ? (
+          <Box sx={{ width: '100%', marginTop:20 }}>
+            <LinearProgress />
+          </Box>
+        ) : (
+          <div style={{ height: 500, width: "100%" }}>
+            <ResponsiveBar
+              data={pivotDataGroup}
+              keys={
+                pivot ? filters[pivot.value].map((obj) => obj.value) : ["score"]
+              }
+              indexBy={axis.value}
+              groupMode="grouped"
+              margin={{ top: 50, right: 130, bottom: 100, left: 60 }}
+              padding={0.3}
+              legends={[
+                {
+                  dataFrom: "keys",
+                  anchor: "bottom-left",
+                  direction: "column",
+                  justify: false,
+                  translateX: 0,
+                  translateY: 100,
+                  itemsSpacing: 2,
+                  itemWidth: 100,
+                  itemHeight: 20,
+                  itemDirection: "left-to-right",
+                  itemOpacity: 0.85,
+                  symbolSize: 20,
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+              tooltip={(point) => (
+                <div style={{ maxWidth: 400, background: "white" }}>
+                  <p>
+                    {point.id}:{point.value}
+                  </p>
                   <p>{point.data.demographic}</p>
                   {/* <pre>{JSON.stringify(point, null, 2)}</pre> */}
-              </div>
-            )}
-          />
-        </div>
+                </div>
+              )}
+            />
+          </div>
+        )}
+
         {/* <pre>{JSON.stringify(pivotDataGroup, null, 2)}</pre> */}
         {/* <pre>{JSON.stringify(filters, null, 2)}</pre> */}
       </Box>
