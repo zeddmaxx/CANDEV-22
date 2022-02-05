8afd0b94f79092d4181cd9d0a7833e5b89631405
michaelzusev <mike.zusev@gmail.com>
2022-02-04 22:20:46 -0500

    Add chart and data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,35 +5,43 @@ import { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
-//Firebase
-import app from "./utils/firebase";
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
+//Nivo
+import { ResponsiveBar } from "@nivo/bar";
 
-const storage = getStorage(app);
 
 const App = () => {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [filters, setFilters] = useState({
+    survey_year: 2020,
+    question:
+      "Question 1. I have the tools, technology and equipment I need to do my job.",
+    department: "Public Service",
+  });
 
   useEffect(() => {
-    setLoading(true)
-    getDownloadURL(ref(storage, "clean.json")).then((url) => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.slice(0,10))
-          setData(data);
-          setLoading(false)
-        })
-    })
-    .catch(err => {
-      console.log(err);
-      setLoading(false)
-    })
+    setLoading(true);
+    fetch('data/clean.json')
+      .then(response => response.json())
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log(err);
+        setLoading(false)
+      })
   }, []);
 
+  const filteredData = data.filter(
+    (point) =>
+      point.survey_year === filters.survey_year &&
+      point.question === filters.question &&
+      point.department === filters.department
+  );
 
   return (
     <Container>
@@ -44,14 +52,16 @@ const App = () => {
         <Typography variant="h5" component="h2" gutterBottom>
           Retention and mobilization of talent among employment equity groups
         </Typography>
-        {
-          loading &&
-          <CircularProgress />
-        }
-        {
-          data &&
-            <pre>{JSON.stringify(data.slice(0,10), null, 2)}</pre>
-        }
+        {loading && !data && <CircularProgress />}
+        <div style={{ height: 200, width: "100%" }}>
+          <ResponsiveBar
+            data={filteredData}
+            keys={["score"]}
+            indexBy="demographic"
+          />
+        </div>
+
+        <pre>{JSON.stringify(filteredData, null, 2)}</pre>
       </Box>
     </Container>
   );
