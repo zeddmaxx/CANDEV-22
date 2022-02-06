e7e30396e85fbad87398bdea7dacdddd3d8102d2
michaelzusev <mike.zusev@gmail.com>
2022-02-06 10:10:39 -0500

    Add more analysis

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -39,13 +39,18 @@ indigenous_non <- data %>%
   filter(department == 'Innovation, Science and Economic Development Canada')
 
 
-indigenous_non <- data %>%
-  select(!c(answer_count, subindicator)) %>%
-  filter(demographic == 'Indigenous'| demographic == "Non-Indigenous") %>%
+disability_non <- data %>%
+  select(!subindicator) %>%
+  filter(demographic == 'Person with a disability'| demographic == "Not a person with a disability") %>%
   group_by(survey_year) %>%
-  pivot_wider(names_from = "demographic", values_from = "score") %>%
-  mutate(difference = `Indigenous` - `Non-Indigenous`) %>% 
-  filter(survey_year == '2020')
+  pivot_wider(names_from = "demographic", values_from = c(score, answer_count)) %>%
+  mutate(difference = `score_Not a person with a disability` - `score_Person with a disability`) %>% 
+  mutate(share = `answer_count_Person with a disability`/`answer_count_Not a person with a disability`) %>%
+  mutate(mean_share = mean(share, na.rm = TRUE)) %>%
+  mutate(share_difference = share/mean_share) %>%
+  filter(survey_year == '2020') %>%
+  filter(department == 'Innovation, Science and Economic Development Canada')
+
 
 disability_non <- data %>%
   select(!c(answer_count, subindicator)) %>%
